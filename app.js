3c17e007bc0a871ad24af00058eb3b91d1f8c8e2
EdgardoSalazar <82413057+EdgardoSalazar@users.noreply.github.com>
2025-10-17 18:26:48 +0000

    revision

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-window.onload = function() {
-
+window.onload = function () {
   let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
   let action = ['comió', 'orinó en', 'aplastó', 'rompió'];
   let what = ['mi tarea', 'mi teléfono', 'el auto'];
   let when = ['antes de clase', 'mientras dormía', 'cuando hacía ejercicio', 'durante el almuerzo', 'mientras rezaba'];
 
- let parte1 = Math.floor(Math.random() * who.length);
- let parte2 = Math.floor(Math.random() * action.length);
- let parte3 = Math.floor(Math.random() * what.length);
- let parte4 = Math.floor(Math.random() * when.length);
-    
-   document.querySelector("#excuse").innerHTML = 
-  who[parte1] + action[parte2] 
- + what[parte3] + when[parte4];
+  let indiceSustantivo = Math.floor(Math.random() * who.length);
+  let indiceAcciones = Math.floor(Math.random() * action.length);
+  let indiceEvitar = Math.floor(Math.random() * what.length);
+  let indiceCuando = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML = who[indiceSustantivo] + action[indiceAcciones] + what[indiceEvitar] + when[indiceCuando];
 };
